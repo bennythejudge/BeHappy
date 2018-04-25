9756587f39420049f86e0e40f1e102907679f59e
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-25 23:32:46 +0200

    Add fragment for quotes

---
+++ app/src/main/java/android/benedetto/com/behappy/data/QuoteViewPagerAdapter.java
@@ -0,0 +1,27 @@
+package android.benedetto.com.behappy.data;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FrameMetricsAggregator;
+
+import java.util.List;
+
+public class QuoteViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+
+    public QuoteViewPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+}
